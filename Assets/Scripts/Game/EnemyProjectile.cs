092125c4b3036443ffd9982d51a6d2141f0a2ac0
Kizaru <Kizaruu30@gmail.com>
2015-11-12 12:00:51 +0100

    FirstRealCommit

---
+++ Assets/Scripts/Game/EnemyProjectile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyProjectile : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+            transform.position.y - speed,
+            transform.position.z);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Limit")
+            Destroy(gameObject);
+        if (col.gameObject.name == "MainMecha")
+            {
+            col.GetComponent<MainMecha>().GetDamage();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/EnemySimpleMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySimpleMove : Enemy {
+
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    public override void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y - moveSpeed, transform.position.z);
+        base.Update();
+	}
+}
