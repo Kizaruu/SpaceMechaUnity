cb4a1798de463f97c796dd7d2b159567fd6134c0
Kizaru <Kizaruu30@gmail.com>
2015-11-13 16:02:59 +0100

    Respawn enemy

---
+++ Assets/Scripts/Game/GameRun.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+using System;
+
+public class GameRun : MonoBehaviour {
+    public GameObject starship;
+    public float time;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("CreateStarship");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private IEnumerator CreateStarship()
+    {
+        while (true)
+        {
+            time -= 0.5F;
+            if (time < 0)
+                time = 0.5F;
+            yield return new WaitForSeconds(time); // todo incrémentation du temps au fur et a mesure
+            Instantiate(starship,
+                new Vector3(UnityEngine.Random.Range(-20, 20),
+                50 + 1,
+                -5), Quaternion.identity);
+                           
+        }
+       
+    }
+}
